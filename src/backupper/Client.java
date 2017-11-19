6306cbf85660491c8b9172efa48ab38dec7be40b
Piotr <Piotr@192.168.0.17>
2017-11-19 15:31:30 +0100

    witam

--- src/backupper/Client.java
+++ src/backupper/Client.java
@@ -5,10 +5,40 @@
  */
 package backupper;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  *
  * @author Piotr
  */
 public class Client {
-    
+    public static void main(String[] args){
+        // ladowanie ustawien klienta z pliku
+        Properties props = new Properties();
+        InputStream input = null;
+        String ip = "localhost";
+        int portnum = 1000;
+        
+        try {
+            input = new FileInputStream("client.properties");
+            props.load(input);
+            ip = props.getProperty("ip");
+            portnum = Integer.parseInt(props.getProperty("ip"));
+        } catch (IOException ex) {
+		ex.printStackTrace();
+	} finally {
+		if (input != null) {
+			try {
+				input.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+        
+        
+    }
 }
