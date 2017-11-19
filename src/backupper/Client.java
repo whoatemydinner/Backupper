c12ee3c11b6d0b01ec65a345cfb3870012c32f05
Piotr <Piotr@192.168.0.17>
2017-11-19 16:41:57 +0100

    witam

--- src/backupper/Client.java
+++ src/backupper/Client.java
@@ -5,28 +5,68 @@
  */
 package backupper;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
 /**
  *
  * @author Piotr
  */
 public class Client {
-    public static void main(String[] args){
+    public static String ip = "localhost";
+    public static int portnum = 1000;
+    public static String fileToSave = "pliktekstowyserwera_otrzymany.txt";
+    public final static String fileToSend = "pliktekstowyklienta.txt";
+    public static int maxFileSize = 6000000; // no nie jest to optymalne, tylko 6 mb
+    
+    private static void receivefile(Socket ssock, FileOutputStream fos, BufferedOutputStream bos) throws IOException{
+        int bytesRead;
+        int currentByte = 0;
+        // sprobujmy otrzymac plik
+        byte[] bytearray = new byte[maxFileSize];
+        InputStream is = ssock.getInputStream();
+        fos = new FileOutputStream(fileToSave);
+        bos = new BufferedOutputStream(fos);
+        bytesRead = is.read(bytearray,0,bytearray.length);
+        currentByte = bytesRead;
+            
+        do {
+            bytesRead = is.read(bytearray, currentByte, (bytearray.length-currentByte));
+            if (bytesRead >= 0) currentByte += bytesRead;
+        } while (bytesRead < -1);
+            
+        bos.write(bytearray, 0, currentByte);
+        bos.flush();
+        System.out.println("Plik zostal zapisany pod nazwa " + fileToSave + " (" + currentByte + "b).");
+    }
+    
+    private static void sendfile(FileInputStream fis, BufferedInputStream bis, OutputStream os, Socket csock) throws IOException{
+        // tutaj wysylamy plik od teraz, serio
+        File mf = new File(fileToSend);
+        // ustalamy dlugosc pliku
+        byte[] bytearray = new byte[(int)mf.length()];
+        fis = new FileInputStream(mf);
+        bis = new BufferedInputStream(fis);
+        bis.read(bytearray,0,bytearray.length);
+        os = csock.getOutputStream();
+        System.out.println("Wysyłanie pliku " + fileToSend + "(wielkość " + bytearray.length + "b)");
+        os.write(bytearray,0,bytearray.length);
+        // spuszczamy bajty
+        os.flush();
+        System.out.println("Zrobione. Z fartem, mordo.");
+    }
+    
+    public static void main(String[] args) throws IOException{
         // ladowanie ustawien klienta z pliku
         Properties props = new Properties();
         InputStream input = null;
-        String ip = "localhost";
-        int portnum = 1000;
         
         try {
             input = new FileInputStream("client.properties");
             props.load(input);
             ip = props.getProperty("ip");
-            portnum = Integer.parseInt(props.getProperty("ip"));
+            portnum = Integer.parseInt(props.getProperty("port"));
         } catch (IOException ex) {
 		ex.printStackTrace();
 	} finally {
@@ -39,6 +79,28 @@ public class Client {
 		}
 	}
         
+        FileInputStream fis = null;
+        BufferedInputStream bis = null;
+        OutputStream os = null;
+        FileOutputStream fos = null;
+        BufferedOutputStream bos = null;
+        Socket ssock = null;
+        
+        try {
+            ssock = new Socket(ip, portnum);
+            System.out.println("Laczenie...");
+            System.out.println("Test z otrzymywaniem pliku...");
+            receivefile(ssock, fos, bos);
+            ssock.close();
+            System.out.println("Test z wysyłaniem pliku...");
+            ssock = new Socket(ip, portnum);
+            sendfile(fis,bis,os,ssock);
+            ssock.close();
+        } finally {
+            if (fos != null) fos.close();
+            if (bos != null) bos.close();
+            if (ssock != null) ssock.close();
+        }
         
     }
 }
